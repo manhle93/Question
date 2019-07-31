d2805c087096f0098e7fa3e9cf82fffe24645e5f
Manh Cuong <lmcuongict@gmail.com>
2019-07-31 23:32:29 +0700

    giao dien v1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('play','PlayController@index')->name('game.play');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
